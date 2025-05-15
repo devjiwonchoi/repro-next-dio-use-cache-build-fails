97fc9452588dfef4ef88e13a074a03dc56f2cd78
devjiwonchoi <devjiwonchoi@gmail.com>
2025-05-15 19:22:17 +0200

    this is fix

--- app/some/[slug]/page.tsx
+++ app/some/[slug]/page.tsx
@@ -1,9 +1,17 @@
+import { unstable_cacheLife as cacheLife } from 'next/cache'
 import { Suspense } from 'react'
 
 async function doSomething(slug: string) {
   'use cache'
   console.log('doSomething', slug)
+  cacheLife('hours')
   return slug
+}
+
+async function Handle({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params
+  const result = await doSomething(slug)
+  return <p>Hello {result}</p>
 }
 
 export default async function Home({
@@ -11,11 +19,9 @@ export default async function Home({
 }: {
   params: Promise<{ slug: string }>
 }) {
-  const { slug } = await params
-  const result = await doSomething(slug)
   return (
     <Suspense fallback={<p>Loading...</p>}>
-      <p>Hello {result}</p>
+      <Handle params={params} />
     </Suspense>
   )
 }
